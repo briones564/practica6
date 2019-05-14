9e93ba9e0afa3c724304ce546228df60166b6cc7
dam08 <dam08@PC08.aula1.com>
2019-05-14 20:05:56 +0200

    ----------------------- COMMIT 1 Proyecto 6 v1.0 Rama ClasePrincipal
    -Añadido método menú (sin funcionalidad).
    ----------------------

--- src/practica6/Practica6.java
+++ src/practica6/Practica6.java
@@ -5,6 +5,8 @@
  */
 package practica6;
 
+import java.util.Scanner;
+
 /**
  *
  * @author dam08
@@ -16,6 +18,33 @@ public class Practica6 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        menu();
     }
     
+    public static void menu () {
+    
+        Scanner scan = new Scanner(System.in);
+        int opcion = 0;
+        
+        System.out.println("Bienvenido al Sistema de Gestión de tu cuenta LA KAIXA\n¿Qué deseas hacer?\n");
+        System.out.println("1. Crear cliente\n2. Crear cuenta\n3. Ingresar dinero\n4. Retirar dinero\n");
+        System.out.print("---> OPCIÓN:_");
+        opcion = scan.nextInt();
+        
+        switch (opcion){
+        
+            case 1:
+                crearCliente();
+                break;
+            case 2:
+                crearCuenta();
+                break;
+            case 3:
+                ingresarDinero();
+                break;
+            case 4:
+                retirarDinero();
+                break;
+        }
+    }
 }
